450cd4367f1f6dfc26532f75d210f7ff14a5e669
Shirlette Chambers <shirlette.chambers@gmail.com>
2016-10-04 13:53:00 -0400

    created user input section for message insertion and data display

--- main.py
+++ main.py
@@ -28,7 +28,12 @@ app.secret_key = "navlksnlakjwa8924hr8qoarhfvpui34"
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	current_posts_query = "SELECT * FROM buzz LEFT JOIN user ON buzz.user_id = user.id ORDER BY date DESC"
+	cursor.execute(current_posts_query)
+	current_posts_result = cursor.fetchall()
+	return render_template('index.html',
+		posts = current_posts_result)
+
 
 @app.route('/register')
 def register():
@@ -61,6 +66,7 @@ def register_submit():
 		username_insert_query = "INSERT INTO user VALUES (default, '"+username+"', '"+email+"', '"+hashed_password+"', null, '"+real_name+"')"
 		cursor.execute(username_insert_query)
 		conn.commit()
+		session['username'] = username
 		return render_template('index.html')
 	else: 
 		return redirect('/register?username=taken')
@@ -80,25 +86,45 @@ def sign_in():
 @app.route('/sign_in_submit', methods=['POST'])
 def sign_in_submit():
  	password = request.form['password'].encode('utf-8')
- 	hashed_password_from_mysql = "SELECT password FROM user WHERE username = '%s' " % request.form['username']
+ 	hashed_password_from_mysql = "SELECT password, id FROM user WHERE username = '%s'" % request.form['username']
  	cursor.execute(hashed_password_from_mysql)
  	hashed_password = cursor.fetchone()
+ 	check_username_result = hashed_password[1]
 	#to check a hash
 	if hashed_password == None:
 		return render_template("/sign_in.html",
- 			message = "Try Again")
+ 			message = "Cannot find you in the system. Try Again")
 	if bcrypt.checkpw(password, hashed_password[0].encode('utf-8')):	
  		session['username'] = request.form['username']
+ 		session['user_id'] = check_username_result
+ 		print session['user_id']
  		return redirect('/')
  	else: 
  		return render_template("/sign_in.html",
- 			message = "Try Again")
+ 			message = "Wrong password. Try Again")
 
 @app.route('/logout')
 def logout():
 	#nuke the session vars. This will end the session which is what we use to let the user in
 	session.clear()
 	return redirect('/sign_in?YouAreLogedOut')
+
+
+@app.route('/post_submit', methods=["POST"])
+def post_submit():
+	post_content = request.form['post_content']
+	get_user_id_query = "SELECT id FROM user WHERE username = '%s'" % session['username']
+	print session['username']
+	cursor.execute(get_user_id_query)
+	get_user_id_result = cursor.fetchone()
+	user_id = get_user_id_result[0]
+	print user_id
+	insert_post_query = "INSERT INTO buzz (user_id, post_content, current_vote) VALUES ("+str(user_id)+", '"+post_content+"', 0)"
+	cursor.execute(insert_post_query)
+	conn.commit()
+	return redirect('/')
+
+
 
 
 if __name__ == "__main__":
